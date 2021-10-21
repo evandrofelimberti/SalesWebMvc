a315afa36a4f2ad7958fea6438b50f6391f06fad
Evandro Felimberti <evandrofelimberti@gmail.com>
2021-10-20 21:34:03 -0300

    Seller view create

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -19,5 +19,10 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller seller)
+        {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
